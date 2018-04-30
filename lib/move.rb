2990a52851fb657595ddf61f244b3777e798b1de
Jon Wenger <jonwenger783@gmail.com>
2018-04-30 19:36:37 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,18 @@
+board = [" "," "," "," "," "," "," "," "," ",]
+
+def input_to_index(input_to_index)
+  index = (input.to_i-1)
+  update_array_at_with(board,index)
+    end
+
+def move(board, index, character = "X")
+
+end
+
+def update_array_at_with(array, index, value = "X")
+  array[index] = value
+end
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
